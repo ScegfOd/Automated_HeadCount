ad32392ad62d5e2d73eb039267cc8f89698df319
Jonathan Combs <JofCombs@gmail.com>
2019-10-27 06:31:26 -0700

    fixed startup

--- app/src/main/java/com/example/automatedheadcount/initialSetUp.java
+++ app/src/main/java/com/example/automatedheadcount/initialSetUp.java
@@ -1,7 +1,9 @@
 package com.example.automatedheadcount;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,18 @@ public class initialSetUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_set_up);
+        SharedPreferences app_data = PreferenceManager.getDefaultSharedPreferences(this);
+        String id = app_data.getString("NETID", "");
+        String email = app_data.getString("EMAIL", "");
+        Intent intent;
+        if(!email.equals("")){
+            intent = new Intent(this, MainAdmin.class);
+            startActivity(intent);
+        }
+        else if(!id.equals("")){
+            intent = new Intent(this, MainStudent.class);
+            startActivity(intent);
+        }
     }
     public void sign_up_button(android.view.View view){ //for student NET ID
         Intent intent = new Intent(this, StudentSignUp.class);
